34e35025c6872903e45ff04d68a5e846a322c17d
ozgungungor <ozgun.gungor>
2025-11-26 23:37:23 +0300

    Initial Commit

---
+++ minefieldsTests/minefieldsTests.swift
@@ -0,0 +1,17 @@
+//
+//  minefieldsTests.swift
+//  minefieldsTests
+//
+//  Created by özgün güngör on 26.11.2025.
+//
+
+import Testing
+@testable import minefields
+
+struct minefieldsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
